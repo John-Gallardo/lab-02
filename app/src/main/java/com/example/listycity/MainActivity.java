e6d90d8a2ab2cc56d725993b28d85bd6480baf57
John Gallardo <johnpatrickgallardo33@gmail.com>
2025-09-10 18:21:12 -0600

    Finished Delete City button, working on Add City

--- app/src/main/java/com/example/listycity/MainActivity.java
+++ app/src/main/java/com/example/listycity/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.listycity;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.Nullable;
@@ -20,6 +24,11 @@ public class MainActivity extends AppCompatActivity {
     ListView cityList;
     ArrayAdapter<String> cityAdapter;
     ArrayList<String> dataList;
+    Button addCity;
+    Button deleteCity;
+    TextView userInput;
+    int selectedIndex = -1;
+    
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -35,5 +44,35 @@ public class MainActivity extends AppCompatActivity {
 
         cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
         cityList.setAdapter(cityAdapter);
+
+        addCity = findViewById(R.id.add_city);
+        deleteCity = findViewById(R.id.delete_city);
+        userInput = findViewById(R.id.user_input);
+        userInput.setVisibility(TextView.GONE);
+
+        cityList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                selectedIndex = position;
+            }
+        });
+
+        addCity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userInput.setVisibility(TextView.VISIBLE);
+            }
+        });
+
+        deleteCity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (selectedIndex != -1) {
+                    dataList.remove(selectedIndex);
+                    cityAdapter.notifyDataSetChanged();
+                    selectedIndex = -1;
+                }
+            }
+        });
     }
 }
